ca1abb4f7bc8ad24d861c4dcce71caa40660ed61
mmmansouri <mansourimedmoez@gmail.com>
2024-01-26 00:20:47 +0100

    Initial commit

---
+++ src/app/ladies/ladies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ladies',
+  standalone: true,
+  imports: [],
+  templateUrl: './ladies.component.html',
+  styleUrl: './ladies.component.scss'
+})
+export class LadiesComponent {
+
+}
